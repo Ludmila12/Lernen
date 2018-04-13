7bef2c18a091ac25013542607284cc231f211dc6
Ludmila12 <37815443+Ludmila12@users.noreply.github.com>
2018-04-13 17:44:18 +0300

    Create Test2.java

---
+++ Test2.java
@@ -0,0 +1,22 @@
+package Lernen;
+
+public class Test2{
+    public static void main(String args[]) {
+      int a = 10;
+       while (a < 21) {
+         System.out.print(a + " ");
+           a++;
+        }
+        System.out.println();
+            for (int b = 10; b < 21; b++) {
+                System.out.print(b + " ");
+            }
+        System.out.println();
+            int c = 9;
+            do {
+                c++;
+                System.out.print(c + " ");
+            } while (c < 20);
+        System.out.println();
+       }
+        }
