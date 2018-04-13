648592f7a1203a4b2280128612512c1a56730a25
Ludmila12 <37815443+Ludmila12@users.noreply.github.com>
2018-04-13 17:45:57 +0300

    Create Test3.java

---
+++ Test3.java
@@ -0,0 +1,10 @@
+package Lernen;
+
+public class Test3 {
+    public static void main(String[] args) {
+        for (int b = 1; b < 100; b++)
+            if (b%3==0)
+            System.out.print(b + " ");
+
+    }
+}
